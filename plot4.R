832b82c3486fba942f0a85f884df3cf554b29dc0
kmac99s <karakickass@gmail.com>
2015-12-11 11:38:31 -0500

    4/4

--- plot4.R
+++ plot4.R
@@ -12,11 +12,29 @@ df<- subset(df, df$Date %in% range)
 
 #Do Plot as a PNG
 
-png("plot3.png", width=504, height=504)
+png("plot4.png", width=504, height=504)
+
+#Call the plots
+par(mfrow=c(2,2))
+
+#Plot the first
+
+plot(df$datetime, df$Global_active_power, type="l", xlab="", ylab="Global Active Power") 
+
+#Plot the second
+
+plot(df$Voltage ~ df$datetime, type="l", xlab="datetime", ylab="Voltage") 
+
+
+#Plot the third
 
 plot(df$Sub_metering_1 ~ df$datetime, type = "l", ylab = "Energy sub metering", xlab = "") 
 lines(df$Sub_metering_2 ~ df$datetime, col="red", type="l")
 lines(df$Sub_metering_3 ~ df$datetime, col="blue", type="l")
-legend("topright", legend=c("Sub_metering_1", "Sub_metering_2","Sub_metering_3"), lty=c(1,1,1), lwd=c(2.5,2.5,2.5),col=c("black","blue","red"))
+legend("topright", legend=c("Sub_metering_1", "Sub_metering_2","Sub_metering_3"), lty=1,col=c("black","blue","red"), box.lwd=0)
+
+#Plot the fourth
+
+plot(df$Global_active_power ~ df$datetime, type="l", xlab = "datetime") 
 
 dev.off()
